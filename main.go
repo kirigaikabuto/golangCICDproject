0608fe8e7460b23d2f2ea33ce5a625509c2bc692
kirigaikabuto <tleugazy98@gmail.com[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A>
2023-07-29 18:37:00 +0600

    sdsdsdsds

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	msg := sayHello("Alice")
+	fmt.Println(msg)
+}
+
+func sayHello(name string) string {
+	return fmt.Sprintf("Hi %s", name)
 }
